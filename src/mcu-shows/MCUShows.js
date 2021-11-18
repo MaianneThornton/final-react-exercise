1be96edbab318e13bf9ce4c55efa85b6ef3b4030
MaianneThornton <maianne.thornton@gmail.com>
2021-11-18 13:59:10 -0500

    Added MCUShows classname to the div inside of the MCUShows component

--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -3,7 +3,8 @@ import "./MCUShows.css";
 function MCUShows (){
   // 1c. Inside the MCUShows function/component, use the RETURN keyword with parentheses to have the MCUShows function/component return the following elements: h1, h2, and 4 p (paragraph) elements. Put the following text in each element. (NOTE: Notice the colon and space after each show name)
   return (
-    <div>
+    // 4a. Add a class name of "MCUShows" to the the div in the MCUShows component
+    <div className="MCUShows">
       {/* 3a. Go back to the MCUShows.js file. Using inline styling in React, Set the font (A.K.A. font family) for the h1 to the "Georgia" font. Also, add an underline to the h1. */}
       <h1 style={{fontFamily: `Georgia`, textDecoration:`underline`}}>Disney+</h1>
       <h2>MCU Show Release Dates</h2>
