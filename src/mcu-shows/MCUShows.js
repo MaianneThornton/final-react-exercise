f8c9d6aeda1356eab57aaa847faa4e12eb855c0b
MaianneThornton <maianne.thornton@gmail.com>
2021-11-18 13:49:22 -0500

    Imported MCUShows.css file, created and exported MCUShows function

---
+++ src/mcu-shows/MCUShows.js
@@ -0,0 +1,16 @@
+import "./MCUShows.css";
+
+function MCUShows (){
+  return (
+    <div>
+      <h1>Disney+</h1>
+      <h2>MCU Show Release Dates</h2>
+      <p>WandaVision: </p>
+      <p>The Falcon and the Winter Soldier: </p>
+      <p>Loki: </p>
+      <p>What If...?: </p>
+      <p>Hawkeye: </p>
+    </div>
+  );
+}
+export default MCUShows;
