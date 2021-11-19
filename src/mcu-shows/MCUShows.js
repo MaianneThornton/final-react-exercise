07e44f4d120bbec8c67d2641b3a31758db825036
MaianneThornton <maianne.thornton@gmail.com>
2021-11-19 09:17:49 -0500

    Exercise (PRE BONUS) is complete

--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -1,6 +1,6 @@
 import "./MCUShows.css";
 
-function MCUShows (){
+function MCUShows ({dates}){
   // 1c. Inside the MCUShows function/component, use the RETURN keyword with parentheses to have the MCUShows function/component return the following elements: h1, h2, and 4 p (paragraph) elements. Put the following text in each element. (NOTE: Notice the colon and space after each show name)
   return (
     // 4a. Add a class name of "MCUShows" to the the div in the MCUShows component
@@ -8,11 +8,12 @@ function MCUShows (){
       {/* 3a. Go back to the MCUShows.js file. Using inline styling in React, Set the font (A.K.A. font family) for the h1 to the "Georgia" font. Also, add an underline to the h1. */}
       <h1 style={{fontFamily: `Georgia`, textDecoration:`underline`}}>Disney+</h1>
       <h2>MCU Show Release Dates</h2>
-      <p>WandaVision: </p>
-      <p>The Falcon and the Winter Soldier: </p>
-      <p>Loki: </p>
-      <p>What If...?: </p>
-      <p>Hawkeye: </p>
+      {/* 5c. Go back to the MCUShows.js file. Plug the values from the dates property, which holds the value of the releaseDates object, in each p element after each appropriate show name. (HINT: The properties created for a component are stored in an object that can be accessed via a parameter in a function component) */}
+      <p>WandaVision: {dates.wandaVision} </p>
+      <p>The Falcon and the Winter Soldier: {dates.falconWinter}</p>
+      <p>Loki: {dates.loki}</p>
+      <p>What If...?: {dates.whatIf}</p>
+      <p>Hawkeye: {dates.hawkeye}</p>
     </div>
   );
 }
