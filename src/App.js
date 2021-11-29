2f5bc779dffee08456237ec089085530524fcfc1
MaianneThornton <maianne.thornton@gmail.com>
2021-11-29 11:49:01 -0500

    Completed Bonus section in class. Exercise is complete.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 // 2a. Open the App.js file and add the necessary import line at the top of the file that allows the MCUShows component to be used in the App.js file
 import MCUShows from "./mcu-shows/MCUShows";
 
+// BONUS: 6a. In the App.js file, import the useState function at the top of the file
+//  and BONUS: 8a
+import { useState, useEffect } from 'react';
+
 // 5a. Below the MCUShows import in the App.js file, create the following variable
 const releaseDates = {
   wandaVision: 'January 2021',
@@ -8,19 +12,66 @@ const releaseDates = {
   loki: 'June 2021',
   whatIf: 'August 2021',
   hawkeye: 'November 2021'
- }; 
+};
+
+//  BONUS: 6b. Create the following array variable below the releaseDates variable:
+const mcuCharacters = [
+  'Scarlet Witch',
+  'Vision',
+  'Falcon',
+  'Winter Soldier',
+  'Loki',
+  'The Watcher',
+  'Hawkeye'
+];
 
 function App() {
+  // BONUS: 6c. Above the return line in the App function component, use the useState function to set the starting state value to 0 (Zero). Also, use destructuring to set the values in the array that is returned from the useState function to the following variable names…
+  const [index, setIndex] = useState(0);
+
+  // BONUS: 7a. In the App function component, use another useState function below the first one and set the starting state value to 0 (Zero) on this one as well. Also, use destructuring to set the values in the array that is returned from the useState function to the following variable names…
+  const [num, setNum] = useState(0);
+
+  // BONUS: 8a. Use the useEffect Hook/Function so that the message "The Next MCU Character has been displayed" displays in an alert when the page loads and also when the "NEXT MCU CHARACTER" button is pressed, but NOT when the "RANDOM MCU CHARACTER" button is pressed.
+  // The function (1st argument) passed into the useEffect function/hook will run every time the state value ("num" in this example) that is passed into the array (2nd argument) is updated.
+  // NOTE: If nothing is passed in for a 2nd argument, then the function will run whenever any state values are updated.
+  useEffect(() => {
+    alert("The Next MCU Character has been displayed");
+  }, [num]);
+  // BONUS 6f
+  function newIndex() {
+    const randomIndex = Math.floor(Math.random() * mcuCharacters.length);
+    setIndex(randomIndex);
+  }
+
+  // BONUS: 7d
+  function newNum() {
+    if (num < mcuCharacters.length - 1) {
+      setNum(num + 1);
+    } else {
+      setNum(0);
+    }
+  }
   return (
     <div>
       {/* 2b. Inside the div in the App component, create an h1 with the following text: "FINAL REACT EXERCISE" */}
       <h1>FINAL REACT EXERCISE</h1>
       {/* 2c. Below the h1 pass in an MCUShows component */}
       {/* 5b. Add a prop called "dates" to the MCUShows component that is being passed into the App.js component. Also, set the releaseDates variable as the value that is passed into the "dates" prop */}
-      <MCUShows dates={releaseDates}/>
+      {/* BONUS 6f */}
+      {/* BONUS: 7d */}
+      <MCUShows
+        dates={releaseDates}
+        indexTwo={newIndex}
+        numTwo={newNum} />
 
       {/* BONUS: 6d. Below the MCUShows component in the App.js file create an h1 element with the following content inside: "Random MCU Character:" */}
-      <h1>Random MCU Character</h1>
+      {/* BONUS 6f */}
+      <h1>Random MCU Character: {mcuCharacters[index]}</h1>
+
+      {/* BONUS: 7b. Below the Random MCU Character h1 in the App.js file create another h1 element with the following content inside: "Next MCU Character:" */}
+      <h1>Next MCU Character: {mcuCharacters[num]}</h1>
+
     </div>
   );
 }
--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -1,12 +1,12 @@
 import "./MCUShows.css";
-// 5c
-function MCUShows ({dates}){
+// 5c & BONUS 6f & {mcuCharacters[index]}
+function MCUShows({ dates, indexTwo, numTwo }) {
   // 1c. Inside the MCUShows function/component, use the RETURN keyword with parentheses to have the MCUShows function/component return the following elements: h1, h2, and 4 p (paragraph) elements. Put the following text in each element. (NOTE: Notice the colon and space after each show name)
   return (
     // 4a. Add a class name of "MCUShows" to the the div in the MCUShows component
     <div className="MCUShows">
       {/* 3a. Go back to the MCUShows.js file. Using inline styling in React, Set the font (A.K.A. font family) for the h1 to the "Georgia" font. Also, add an underline to the h1. */}
-      <h1 style={{fontFamily: `Georgia`, textDecoration:`underline`}}>Disney+</h1>
+      <h1 style={{ fontFamily: `Georgia`, textDecoration: `underline` }}>Disney+</h1>
       <h2>MCU Show Release Dates</h2>
       {/* 5c. Go back to the MCUShows.js file. Plug the values from the dates property, which holds the value of the releaseDates object, in each p element after each appropriate show name. (HINT: The properties created for a component are stored in an object that can be accessed via a parameter in a function component) */}
       <p>WandaVision: {dates.wandaVision} </p>
@@ -14,6 +14,13 @@ function MCUShows ({dates}){
       <p>Loki: {dates.loki}</p>
       <p>What If...?: {dates.whatIf}</p>
       <p>Hawkeye: {dates.hawkeye}</p>
+      {/* BONUS: 6e. In the MCUShows component create a button below the final p (paragraph) element. Set the content/text for the button to the following text: "RANDOM MCU CHARACTER". */}
+      {/* BONUS: 6f. Have a random MCU character name display in the new h1 element after "Random MCU Character: " whenever the RANDOM MCU CHARACTER button is clicked. */}
+      <button onClick={indexTwo}>RANDOM MCU CHARACTER</button>
+
+      {/* BONUS: 7c. In the MCUShows component create another button below the "RANDOM MCU CHARACTER" button. Set the content/text for the button to the following text: "NEXT MCU CHARACTER". */}
+      {/* BONUS: 7d. Have an MCU Character name display in the new h1 element after "Next MCU Character: " whenever the NEXT MCU CHARACTER button is clicked. Have this set up so that it will start with "Scarlet Witch" when the page loads and every time the NEXT MCU CHARACTER button is clicked have it display whichever MCU Character name comes next in the "mcuCharacters" array. Also, whenever it gets to "Hawkeye", who is the last name in the array, have it start back at "Scarlet Witch" when the button is pressed again. */}
+      <button onClick={numTwo}>NEXT MCU CHARACTER</button>
     </div>
   );
 }
